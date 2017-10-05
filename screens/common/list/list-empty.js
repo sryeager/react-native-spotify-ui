175ffd969aaaacd5692922a4fdc04a0b41638bcd
Drew Volz <volz.drew@gmail.com>
2017-10-04 22:56:25 -0400

    Include layout and list components

---
+++ screens/common/list/list-empty.js
@@ -0,0 +1,17 @@
+// @flow
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export class ListEmpty extends React.PureComponent {
+  props: {
+    mode: 'bug',
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>List is empty</Text>
+      </View>
+    )
+  }
+}
