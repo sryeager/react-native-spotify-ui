6d91ee6f2b152691469fb01eb589a8ba4a479732
Drew Volz <volz.drew@gmail.com>
2017-10-04 22:04:18 -0400

    Additional routes

---
+++ screens/Search/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import {View, ScrollView, Text, StyleSheet} from 'react-native'
+import Header from '../common/header'
+import {TOGETHER} from '../common/footer'
+
+export default () => {
+  return (
+    <View style={styles.container}>
+      <View style={{height: TOGETHER}} />
+      <Header name="SEARCH" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+})
---
+++ screens/routes.js
@@ -0,0 +1,13 @@
+import Home from './Home'
+import Browse from './Browse'
+import Search from './Search'
+import Radio from './Radio'
+import Library from './Library'
+
+export const ROUTES = {
+  Home: Home,
+  Browse: Browse,
+  Search: Search,
+  Radio: Radio,
+  Library: Library,
+}
